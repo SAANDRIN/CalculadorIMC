b17d00f655d78cfd23a362e956fb4b0b262b98da
devsandrin <a.silvanj@hotmail.com>
2024-09-09 20:35:50 -0300

    Criando a interface gráfica

--- app/src/main/java/com/sandrin/calculadorimc/MainActivity.kt
+++ app/src/main/java/com/sandrin/calculadorimc/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.sandrin.calculadorimc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+
+            val intent = Intent(this, ResultadoActivity::class.java)
+
+            startActivity( intent )
+
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/sandrin/calculadorimc/ResultadoActivity.kt
@@ -0,0 +1,13 @@
+package com.sandrin.calculadorimc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+    }
+}
